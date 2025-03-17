06286e3fc0517f1c4513b662b13320538e78f061
gurjap <gurjaph@my.yorku.ca>
2025-03-17 01:59:34 -0400

    Implement courses API with CRUD operations and update database connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,29 +1,26 @@
-
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = process.env.ATLAS_URI || "";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 try {
-  // Connect the client to the server	(optional starting in v4.7)
+  // Connect the client to the server
   await client.connect();
   // Send a ping to confirm a successful connection
   await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (e) {
-  console.error(e);
-} finally {
-  // Ensures that the client will close when you finish/error
-  await client.close();
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
 }
 
-let db = client.db("courses");
+let db = client.db("CourseCompass");
 
 export default db;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import cors from 'cors';
 import express from 'express';
-import records from './routes/record.js';
+import courses from './routes/courses.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/",records);
+app.use("/courses",courses);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
